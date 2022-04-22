e6f324fe4b0ff1f73f2120a6657c4cd4caf8f589
BackdevHong <ghddls0704@kakao.com>
2022-04-22 23:33:43 +0900

    learning Datefns

--- chap3(lib)/date/src/App.js
+++ chap3(lib)/date/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 // import MomentExample from './components/Moment/MomentExample';
 // import DayjsExample from './components/dayjs/DayjsExample'
+import DateFnsExample from './components/DateFns/DateFnsExample'
 
 function App() {
   return (
     <div className="App">
+      <DateFnsExample></DateFnsExample>
       {/* <MomentExample /> Moment 라이브러리 */}
       {/* <DayjsExample></DayjsExample> Dayjs 라이브러리 */}
     </div>
