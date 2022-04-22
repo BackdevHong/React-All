d76c111b4036ec23e5fed36214a854e7920a01c0
BackdevHong <ghddls0704@kakao.com>
2022-04-22 23:47:53 +0900

    Date 라이브러리 종료!

--- chap3(lib)/date/src/components/DateFns/DateFnsExample.jsx
+++ chap3(lib)/date/src/components/DateFns/DateFnsExample.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
-import { add, format, sub } from 'date-fns'
+import { add, format, sub, differenceInHours } from 'date-fns'
+import { format as timezoneFormat } from 'date-fns-tz';
 import { ko } from 'date-fns/locale'
 
 export default function MomentExample() {
@@ -43,23 +44,22 @@ export default function MomentExample() {
             <input type="date" ref={birthDayRef} onChange={handleBirthDayChange} />
             <div>{day}</div>
             <br />
-            {/* <h1>날짜를 비교해보자!</h1>
+             <h1>날짜를 비교해보자!</h1>
             <div>
                 <p>2022년 4월 21일 22시 09분과 2022년 4월 22일 12시 00분은 몇시간 차이가 있을까?</p>
-                <div>{`${dayjs("2022-04-21 22:09").diff(dayjs("2022-04-22 12:00"),"hours")}시간`}</div>
-            </div>
-            <br />
-            <h1>타임존 확인!</h1>
-            <div>
-                {dayjs.tz.guess()}
+                <div>{differenceInHours(new Date("2022-04-21 22:09:00"), new Date("2022-04-22 12:00:00"))}시간</div>
             </div>
             <br />
             <h1>타임존 표현</h1>
             <div>
-                {dayjs.tz("2022-04-21 22:33:00", "America/New_York").format()}
+                {format(newDateFnsDate, "yyyy-MM-dd HH:mm:ss", { timeZone : 'US/Central'})}
                 <br />
-                {dayjs.tz.guess()}
-            </div> */}
+            </div>
+            <br />
+            <h1>또 다른 타임존 표현</h1>
+            <div>
+                {timezoneFormat(add(new Date("2022-04-22 23:43:00"), {days: 2}), 'yyyy-MM-dd HH:mm:ssXXX', {timeZone: "America/New_York"})}
+            </div>
         </div>
     )
 }
