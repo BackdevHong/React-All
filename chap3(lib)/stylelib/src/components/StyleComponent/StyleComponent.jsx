45d6858a083fbc96c46a3468dae2a73c974214ea
BackdevHong <ghddls0704@kakao.com>
2022-04-23 20:42:55 +0900

    StyledComponent 야무지다 ㄹㅇ

---
+++ chap3(lib)/stylelib/src/components/StyleComponent/StyleComponent.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function StyleComponent() {
+    const Title = styled.h1`
+        font-size: 1.5em;
+        text-align: center;
+        color: palevioletred;
+    `;
+
+    const Wrapper = styled.section`
+        padding: 4em;
+        background: papayawhip;
+    `;
+
+    return(
+    <Wrapper>
+            <Title>
+                Hello World! 
+            </Title>
+    </Wrapper>
+    );
+}
