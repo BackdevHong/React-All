5b60d6acf11ad89fd39ad0a3c6505a12dec2f0cf
BackdevHong <ghddls0704@kakao.com>
2022-04-23 23:26:42 +0900

    asdf

--- chap3(lib)/stylelib/src/components/StyleComponent/StyleComponent.jsx
+++ chap3(lib)/stylelib/src/components/StyleComponent/StyleComponent.jsx
@@ -12,12 +12,38 @@ export default function StyleComponent() {
         padding: 4em;
         background: papayawhip;
     `;
+    const Button = styled.button`
+    /* Adapt the colors based on primary prop */
+        background: ${props => props.primary ? "palevioletred" : "white"};
+        color: ${props => props.primary ? "white" : "palevioletred"};
+        font-size: 1em;
+        margin: 1em;
+        padding: 0.25em 1em;
+        border: 2px solid palevioletred;
+        border-radius: 3px;
+    `;
 
-    return(
+    const TomatoButton = styled(Button)`
+        color: tomato;
+        border-color: tomato;
+    `;
+
+    const ReversedButton = ( props ) => (
+        <Button {...props} children={props.children.split('').reverse()} />
+    )
+
+    return (
     <Wrapper>
-            <Title>
-                Hello World! 
-            </Title>
+        <Title>
+            Hello World! 
+        </Title>
+        <Button onClick={() => {alert("Normal")}}>Normal</Button>
+        <Button onClick={() => {alert("Primary")}} primary>Primary</Button>
+        <TomatoButton>New Component</TomatoButton>
+        <br />
+        <Button as="a" href="#">Link with Button styles</Button>
+        <TomatoButton as="a" href="#">Link with Tomato Button styles</TomatoButton>
+        <Button as={ReversedButton}>Custom Button with Reverse</Button>
     </Wrapper>
     );
 }
