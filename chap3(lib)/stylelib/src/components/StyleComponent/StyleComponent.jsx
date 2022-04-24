7ad9c1e9130207a34b6c6de9e9e1cacb01da504e
BackdevHong <ghddls0704@kakao.com>
2022-04-24 21:24:41 +0900

    StyleIsGod

--- chap3(lib)/stylelib/src/components/StyleComponent/StyleComponent.jsx
+++ chap3(lib)/stylelib/src/components/StyleComponent/StyleComponent.jsx
@@ -1,40 +1,105 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export default function StyleComponent() {
-    const Title = styled.h1`
-        font-size: 1.5em;
-        text-align: center;
-        color: palevioletred;
-    `;
+    // const Title = styled.h1`
+    //     font-size: 1.5em;
+    //     text-align: center;
+    //     color: palevioletred;
+    // `;
 
     const Wrapper = styled.section`
         padding: 4em;
         background: papayawhip;
     `;
-    const Button = styled.button`
-    /* Adapt the colors based on primary prop */
-        background: ${props => props.primary ? "palevioletred" : "white"};
-        color: ${props => props.primary ? "white" : "palevioletred"};
-        font-size: 1em;
-        margin: 1em;
-        padding: 0.25em 1em;
-        border: 2px solid palevioletred;
-        border-radius: 3px;
+    // const Button = styled.button`
+    // /* Adapt the colors based on primary prop */
+    //     background: ${props => props.primary ? "palevioletred" : "white"};
+    //     color: ${props => props.primary ? "white" : "palevioletred"};
+    //     font-size: 1em;
+    //     margin: 1em;
+    //     padding: 0.25em 1em;
+    //     border: 2px solid palevioletred;
+    //     border-radius: 3px;
+    // `;
+
+    // const TomatoButton = styled(Button)`
+    //     color: tomato;
+    //     border-color: tomato;
+    // `;
+
+    // const Thing = styled.div.attrs((/* props */) => ({ tabIndex: 0 }))`
+    //     color: blue;
+
+    //     &:hover {
+    //         color: red; // <Thing> when hovered
+    //     }
+
+    //     & ~ & {
+    //         background: tomato; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
+    //     }
+
+    //     & + & {
+    //         background: lime; // <Thing> next to <Thing>
+    //     }
+
+    //     &.something {
+    //         background: orange; // <Thing> tagged with an additional CSS class ".something"
+    //     }
+
+    //     .something-else & {
+    //         border: 1px solid; // <Thing> inside another element labeled ".something-else"
+    //     }
+    // `
+
+    // const Input = styled.input.attrs(props => ({
+    //     // we can define static props
+    //     type: "text",
+
+    //     // or we can define dynamic ones
+    //     size: props.size || "1em",
+    //     }))`
+    //     color: palevioletred;
+    //     font-size: 1em;
+    //     border: 2px solid palevioletred;
+    //     border-radius: 3px;
+
+    //     /* here we use the dynamically computed prop */
+    //     margin: ${props => props.size};
+    //     padding: ${props => props.size};
+    // `;
+
+    // const PasswordInput = styled(Input).attrs({
+    //     type: "password",
+    // })`
+    //     // similarly, border will override Input's border
+    //     border: 2px solid aqua;
+    // `;
+    // const ReversedButton = ( props ) => (
+    //     <Button {...props} children={props.children.split('').reverse()} />
+    // )
+
+    const rotate = keyframes`
+        from {
+            transform: rotate(0deg);
+        }
+
+        to {
+            transform: rotate(360deg);
+        }
     `;
 
-    const TomatoButton = styled(Button)`
-        color: tomato;
-        border-color: tomato;
+    // Here we create a component that will rotate everything we pass in over two seconds
+    const Rotate = styled.div`
+        display: inline-block;
+        animation: ${rotate} 2s linear infinite;
+        padding: 2rem 1rem;
+        font-size: 1.2rem;
     `;
-
-    const ReversedButton = ( props ) => (
-        <Button {...props} children={props.children.split('').reverse()} />
-    )
 
     return (
     <Wrapper>
-        <Title>
+        {/* <Title>
             Hello World! 
         </Title>
         <Button onClick={() => {alert("Normal")}}>Normal</Button>
@@ -43,7 +108,20 @@ export default function StyleComponent() {
         <br />
         <Button as="a" href="#">Link with Button styles</Button>
         <TomatoButton as="a" href="#">Link with Tomato Button styles</TomatoButton>
-        <Button as={ReversedButton}>Custom Button with Reverse</Button>
+            <Button as={ReversedButton}>Custom Button with Reverse</Button>
+        <Thing>Hello world!</Thing>
+        <Thing>How ya doing?</Thing>
+        <Thing className="something">The sun is shining...</Thing>
+        <div>Pretty nice day today.</div>
+            <Thing>Don't you think?</Thing>
+        <div className="something-else">
+            <Thing>Splendid.</Thing>
+            </div>
+        <br />
+        <Input placeholder="A bigger text input" size="2em" />
+        <br />
+        <PasswordInput placeholder="A bigger password input" size="2em" /> */}
+        <Rotate>&lt; 🎠 &gt;</Rotate>
     </Wrapper>
     );
 }
