72331748534d4e512c429b18f2d997258ec73bfb
HongInSung <ghddls0704@kakao.com>
2022-04-29 13:57:34 +0900

    Scss is Good Style Lib

--- chap3(lib)/sass/src/scss/Example.jsx
+++ chap3(lib)/sass/src/scss/Example.jsx
@@ -1,11 +1,38 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./Example.scss";
 
 export default function Example() {
 	return (
-		<div>
-			<p>Example</p>
-			<p className="font">Example</p>
-		</div>
+		<>
+			<div>
+				<p>Example</p>
+				<p className="base">Example</p>
+			</div>
+			<nav>
+				<ul>
+					<li>
+						<a href="#">안녕</a>
+					</li>
+					<li>
+						<a href="#">인평</a>
+					</li>
+					<li>
+						<a href="#">리엑트</a>
+					</li>
+					<li>
+						<a href="#">짱짱맨</a>
+					</li>
+				</ul>
+			</nav>
+			<div>
+				<p className="info">Example</p>
+				<p className="test">Example</p>
+				<p className="success">Example</p>
+				<p className="success2">성공!</p>
+				<p className="warning">경고!</p>
+				<p className="error">에러!</p>
+			</div>
+		</>
 	);
 }
