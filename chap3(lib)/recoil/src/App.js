0ad3a614a49972bee5f5a8d97557406fb27c71d8
BackdevHong <ghddls0704@kakao.com>
2022-05-06 18:34:27 +0900

    hehheh todolist

--- chap3(lib)/recoil/src/App.js
+++ chap3(lib)/recoil/src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
 import { RecoilRoot } from "recoil";
-import FontButton from "./components/recoilExample/FontButton";
-import Text from "./components/recoilExample/Text";
+import TodoList from "./components/TodoExample/TodoList";
+// import FontButton from "./components/recoilExample/FontButton";
+// import Text from "./components/recoilExample/Text";
+// import Charactor from "./components/recoilExample/Charactor";
+
 
 function App() {
-	return (
-		<div className="App">
-			<RecoilRoot>
-				<FontButton></FontButton>
-				<Text></Text>
-			</RecoilRoot>
-		</div>
-	);
+    return (
+        <div className="App">
+            <RecoilRoot>
+                {/* <FontButton></FontButton>
+                <Text></Text>
+                <Charactor></Charactor> */}
+                <TodoList></TodoList>
+            </RecoilRoot>
+        </div>
+    );
 }
 
 export default App;
