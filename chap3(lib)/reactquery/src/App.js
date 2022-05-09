b8dc1f588108bb119ddfab7d8652b968dfcde19d
HongInSung <ghddls0704@kakao.com>
2022-05-09 12:11:55 +0900

    reactQuery

---
+++ chap3(lib)/reactquery/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Example from "./components/ReactQueryExample/Example";
+
+function App() {
+	return (
+		<div className="App">
+			<Example></Example>
+		</div>
+	);
+}
+
+export default App;
