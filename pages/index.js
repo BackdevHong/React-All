ba0e44463443a3a4b926e4218d0c2f617d3375f5
HongInSung <ghddls0704@kakao.com>
2022-05-25 11:41:29 +0900

    고고

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,27 @@ export default function Home() {
 				</p>
 				<p>다시한번.. 앞으로 잘 부탁드립니다..ㅎㅎ</p>
 			</section>
+			<section className={utilStyles.headingMd}>
+				<h2>프로젝트, 포트폴리오</h2>
+				<ul>
+					<li>
+						1. 알고리즘 정리 -
+						<Link href="https://docs.google.com/document/d/1d4c1lsQ41Nx8p1jAWJIJ1_DX_C0t6YI5iYB4cbtMUyo/edit?usp=sharing">
+							링크
+						</Link>
+					</li>
+					<li>
+						2. Vue로 만든 Todo 사이트 -
+						<Link href="https://vuetodo.dothome.com">
+							가보기(현재 서비스 종료)
+						</Link>
+					</li>
+					<li>
+						3. 클린 코드 정리 -
+						<Link href="https://www.notion.so/1-Clean-Code-9ff45d7137d84962a1af8fa76aad4470"></Link>
+					</li>
+				</ul>
+			</section>
 		</Layout>
 	);
 }
